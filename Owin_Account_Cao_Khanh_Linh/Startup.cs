1e35db53dff00372bb922cbb9de98cf050a02f30
linglingg123 <83108209+linglingg123@users.noreply.github.com>
2021-08-24 14:31:12 +0700

    Add files via upload

---
+++ Owin_Account_Cao_Khanh_Linh/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Owin_Account_Cao_Khanh_Linh
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appbuilder)
+        {
+            //System.Web.Http
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(name: "Owin",
+                routeTemplate: "Owin/{Controller}/{id}",
+                defaults: new { id = RouteParameter.Optional });
+            appbuilder.UseWebApi(config);
+        }
+    }
+}
